29ea21051a1d9f56225d18a8cb1a52dea1a3b8af
vladimir-bachilo <v.bachilo.work@gmail.com>
2023-10-27 23:29:41 +0300

    feat(page): add media coverage section

--- pages/works/melio.js
+++ pages/works/melio.js
@@ -4,7 +4,10 @@ import {
   Link,
   List,
   ListItem,
-  Divider
+  Divider,
+  Heading,
+  Center,
+  UnorderedList
 } from '@chakra-ui/react'
 import { ExternalLinkIcon } from '@chakra-ui/icons'
 import { Title, WorkImage, Meta } from '../../components/work'
@@ -51,6 +54,29 @@ const Work = () => (
           </Link>
         </ListItem>
       </List>
+
+      <Heading as="h4" fontSize={16} my={6}>
+        <Center>Media coverage</Center>
+      </Heading>
+
+      <UnorderedList my={4}>
+        <ListItem>
+          <Link href="https://thefinancialtechnologyreport.com/the-top-100-financial-technology-companies-of-2023/">
+            <Badge mr={2}>The Financial TR</Badge>
+            The Top 100 Financial Technology Companies of 2023
+            <ExternalLinkIcon mx="2px" />
+          </Link>
+        </ListItem>
+
+        <ListItem>
+          <Link href="https://www.businesswire.com/news/home/20231023277793/en/">
+            <Badge mr={2}>Businesswire</Badge>
+            Fiserv and Melio Partner to Streamline Accounts Payable and
+            Receivable for Small Businesses
+            <ExternalLinkIcon mx="2px" />
+          </Link>
+        </ListItem>
+      </UnorderedList>
 
       <WorkImage src="/images/works/melio-01.png" alt="Melio" />
       <WorkImage src="/images/works/melio-02.png" alt="Melio" />
